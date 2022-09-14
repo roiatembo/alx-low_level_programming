a9d44b688435449fb4c805f7f1894b917a95ca72
Roia Tembo <roiatembo@gmail.com>
2022-09-14 04:52:01 +0200

    use putchar to print a word

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,7 +11,8 @@ int main(void)
 	int numbers[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int i;
 	for (i = 0; i < 8; i++)
-    putchar(numbers[i]);
+
+		putchar(numbers[i]);
 	putchar('\n');
 	return (0);
 }
